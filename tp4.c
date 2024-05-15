12abe57e58415776e1ce03e74560d0d68e0c3e1e
Mathis Millot <mathismillot05@gmail.com>
2024-05-15 11:06:37 +0200

    1 er commit

---
+++ tp4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct T_Sommet{
+    int borneInf;
+    int bornreSup;
+    struct T_Sommet *filsGauche;
+    struct T_Sommet *filsDroit;
+} T_Sommet;
+
+typedef T_Arbre *T_Sommet;
+

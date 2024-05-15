58c8ae433d1b87cc978ef89091a3cb537b3b28e8
Mathis Millot <mathismillot05@gmail.com>
2024-05-15 12:11:09 +0200

    2ere fonctions

--- tp4.c
+++ tp4.c
@@ -14,7 +14,47 @@ typedef struct T_Sommet{
 typedef T_Sommet* T_Arbre;
 
 
-T_Sommet *creerSommet(int element)
-//ff
+T_Sommet *creerSommet(int element){
+    T_sommet *sommet = (T_Sommet *)malloc(sizeof(T_Sommet));
+    sommet->borneInf = element;
+    sommet->borneSup = element;
+    sommet->filsGauche = NULL;
+    sommet->filsDroit = NULL;
+}
 
-T_Arbre insererElement(T_Arbre abr, int element)
+T_Arbre insererElement(T_Arbre abr, int element){
+    if (abr == NULL){
+        return creerSommet(element);
+    }
+    if (element + 1 < abr->borneInf){
+        T_sommet *nouveauSommet = creerSommet(element);
+        abr->filsGauche = nouveauSommet;
+    }
+    if (abr->borneSup + 1 < element){
+        T_sommet *nouveauSommet = creerSommet(element);
+        abr->filsDroit = nouveauSommet;
+    }
+    else{
+        abr->borneInf = min(abr->borneInf, element);
+        abr->borneSup = max(abr->borneSup, element);
+    }
+}
+
+T_Arbre insererElement(T_Arbre abr, int element){
+    if (abr == NULL){
+        return creerSommet(element);
+    }
+    if (element + 1 < abr->borneInf){
+        T_sommet *nouveauSommet = creerSommet(element);
+        abr->filsGauche = nouveauSommet;
+    }
+    if (abr->borneSup + 1 < element){
+        T_sommet *nouveauSommet = creerSommet(element);
+        abr->filsDroit = nouveauSommet;
+    }
+    else{
+        abr->borneInf = min(abr->borneInf, element);
+        abr->borneSup = max(abr->borneSup, element);
+    }
+}
+

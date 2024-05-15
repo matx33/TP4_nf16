fb9adc2a92abc17be219ea3f059f3f04394ae394
Mathis Millot <mathismillot05@gmail.com>
2024-05-15 23:10:44 +0200

    test fonction supprimer valide

--- tp4.c
+++ tp4.c
@@ -191,4 +191,26 @@ void tailleMemoire(T_Arbre abr) {
     printf("Nombre d'octets gagnés par la représentation par intervalles: %zu octets\n", tailleABRClass - tailleABRIntervalle);
 }
 
+int main(){
+    T_Arbre abr = NULL;
+    abr = insererElement(abr, 1);
+    abr = insererElement(abr, 2);
+    abr = insererElement(abr, 3);
+    abr = insererElement(abr, 6);
+    abr = insererElement(abr, 10);
+    abr = insererElement(abr, 11);
+    abr = insererElement(abr, 12);
+    T_Sommet *sommet = rechercherElement(abr, 10);
+    afficherSommets(abr);
+    printf("Sommet trouve: [%d; %d]\n", sommet->borneInf, sommet->borneSup);
+    abr = supprimerElement(abr, 10);
+    afficherSommets(abr);
+    printf("----------------\n");
+    abr = supprimerElement(abr, 1);
+    afficherSommets(abr);
+    abr = supprimerElement(abr, 6);
+    printf("----------------\n");
+    afficherSommets(abr);
+    return 0;
+}
 
